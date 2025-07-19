fc22935c5e636ea4c140893f072fb815a38a1953
GorbunovIvan <110302058+GorbunovIvan@users.noreply.github.com>
2025-07-19 09:45:27 +0300

    swagger is added

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/swagger.html","/swagger-ui/**", "/swagger-ui.html", "/docs/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .with(jwtConfigurer, customizer -> customizer.setBuilder(http))
